6b5c5fde3e49de12783fd138fc3c83d7d46e1bd9
lbsafe <lbsafe@naver.com>
2023-11-02 16:09:16 +0900

    대나무 만들기 실습중

--- three_study/common/js/index7.js
+++ three_study/common/js/index7.js
@@ -10,7 +10,7 @@ if(WebGL.isWebGLAvailable()){
 
     // camera
     const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.1, 1000);
-    camera.position.set(5,5,5);
+    camera.position.set(10,10,10);
     camera.lookAt(0,0,0);
 
     // randerer
@@ -27,34 +27,86 @@ if(WebGL.isWebGLAvailable()){
 
     // --------------------사과--------------------//
     // 사과 몸통
-    const apple_meterial = new THREE.MeshStandardMaterial({
-        color : 0xe63946
-    });
-    const apple_geo = new THREE.SphereGeometry(1.2, 64, 32);
-    const apple = new THREE.Mesh(apple_geo, apple_meterial);
-    scene.add(apple);
-    // 나무
-    const wood_meterial = new THREE.MeshStandardMaterial({
-        color : 0x99582a
-    });
-    const wood_geo = new THREE.CylinderGeometry(0.15,0.1,0.5,13);
-    const wood = new THREE.Mesh(wood_geo, wood_meterial);
-    scene.add(wood);
-    wood.position.set(0,1.4,0);
-    // 나뭇잎
-    const leaf_meterial = new THREE.MeshStandardMaterial({
-        color : 0xa7c957,
-        side : THREE.DoubleSide
-    });
-    const leaf_geo = new THREE.SphereGeometry(0.6,32,16,0,Math.PI/3);
-    const leaf = new THREE.Mesh(leaf_geo, leaf_meterial);
-    scene.add(leaf);
-    leaf.position.set(0.25,1,-0.6);
-    leaf.rotation.x = THREE.MathUtils.degToRad(50);
-    leaf.rotation.y = THREE.MathUtils.degToRad(230);
-    leaf.rotation.z = THREE.MathUtils.degToRad(-30);
-
+    // const apple_meterial = new THREE.MeshStandardMaterial({
+    //     color : 0xe63946
+    // });
+    // const apple_geo = new THREE.SphereGeometry(1.2, 64, 32);
+    // const apple = new THREE.Mesh(apple_geo, apple_meterial);
+    // scene.add(apple);
+    // // 나무
+    // const wood_meterial = new THREE.MeshStandardMaterial({
+    //     color : 0x99582a
+    // });
+    // const wood_geo = new THREE.CylinderGeometry(0.15,0.1,0.5,13);
+    // const wood = new THREE.Mesh(wood_geo, wood_meterial);
+    // scene.add(wood);
+    // wood.position.set(0,1.4,0);
+    // // 나뭇잎
+    // const leaf_meterial = new THREE.MeshStandardMaterial({
+    //     color : 0xa7c957,
+    //     side : THREE.DoubleSide
+    // });
+    // const leaf_geo = new THREE.SphereGeometry(0.6,32,16,0,Math.PI/3);
+    // const leaf = new THREE.Mesh(leaf_geo, leaf_meterial);
+    // scene.add(leaf);
+    // leaf.position.set(0.25,1,-0.6);
+    // leaf.rotation.x = THREE.MathUtils.degToRad(50);
+    // leaf.rotation.y = THREE.MathUtils.degToRad(230);
+    // leaf.rotation.z = THREE.MathUtils.degToRad(-30);
     // --------------------사과 fin--------------------//
+
+    // --------------------대나무--------------------//
+
+    const bamboo_meterial = new THREE.MeshStandardMaterial({
+        color : 0x6a994e
+    });
+
+    // bamboo1
+    const bamboo_geo1 = new THREE.CylinderGeometry(0.2,0.25,1,32);
+    const bamboo1 = new THREE.Mesh(bamboo_geo1, bamboo_meterial);
+    bamboo1.position.set(0,0,0);
+    scene.add(bamboo1);
+
+    // bamboo2
+    const bamboo_geo2 = new THREE.CylinderGeometry(0.25,0.2,1,32);
+    const bamboo2 = new THREE.Mesh(bamboo_geo2, bamboo_meterial);
+    bamboo2.position.set(0,1,0);
+    scene.add(bamboo2);
+
+    // bamboo3
+    const bamboo3 = new THREE.Mesh(bamboo_geo1, bamboo_meterial);
+    bamboo3.position.set(0,2,0);
+    scene.add(bamboo3);
+
+    // bamboo4
+    const bamboo4 = new THREE.Mesh(bamboo_geo2, bamboo_meterial);
+    bamboo4.position.set(0,3,0);
+    scene.add(bamboo4);
+
+    // bamboo5
+    const bamboo5 = new THREE.Mesh(bamboo_geo1, bamboo_meterial);
+    bamboo5.position.set(0,4,0);
+    scene.add(bamboo5);
+
+    // bamboo6
+    const bamboo6 = new THREE.Mesh(bamboo_geo2, bamboo_meterial);
+    bamboo6.position.set(0,5,0);
+    scene.add(bamboo6);
+
+
+    const bamboo_meterial2 = new THREE.MeshStandardMaterial({
+        color : 0xa7c957
+    });
+    const bamboo_stem = new THREE.CylinderGeometry(0.25,0.25,0.1,32);
+    
+    const bamboo_stem1 = new THREE.Mesh(bamboo_stem, bamboo_meterial2);
+    bamboo_stem1.position.set(0,1.5,0);
+    scene.add(bamboo_stem1);
+    
+    const bamboo_stem2 = new THREE.Mesh(bamboo_stem, bamboo_meterial2);
+    bamboo_stem2.position.set(0,3.5,0);
+    scene.add(bamboo_stem2);
+
 
 
     // axesHelper
